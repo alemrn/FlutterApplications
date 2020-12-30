e266aacdb265e87429a5b78ddf2caddf38405676
alemrn <amorenoechevarria@gmail.com>
2020-12-30 14:22:57 +0100

    login bloc page

--- formvalidation/lib/src/blocs/login_block.dart
+++ formvalidation/lib/src/blocs/login_block.dart
@@ -1,13 +1,17 @@
 import 'dart:async';
 
-class LoginBloc {
+import 'package:formvalidation/src/blocs/validator.dart';
+
+class LoginBloc with validator {
   final _emailController = StreamController<String>.broadcast();
   final _passwordController = StreamController<String>.broadcast();
 
   //Reuperar los datos del Stream
 
-  Stream<String> get emailStream => _emailController.stream;
-  Stream<String> get passwordStream => _passwordController.stream;
+  Stream<String> get emailStream =>
+      _emailController.stream.transform(validarEmail);
+  Stream<String> get passwordStream =>
+      _passwordController.stream.transform(validarPassword);
 
   //Insertar valores al stream
   get changeEmail => _emailController.sink.add;
