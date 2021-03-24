51e7eccf96f899be40940bccc83ec356dab18f69
alemrn <a.moreno@eimagina.net>
2021-03-24 08:07:23 +0100

    pushNotificationApp

---
+++ pushnotification/notificationapp/lib/src/pages/mensaje_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MensajePage extends StatelessWidget {
+  const MensajePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Mensaje page'),
+      ),
+    );
+  }
+}
