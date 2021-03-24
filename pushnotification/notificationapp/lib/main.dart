21512db5fd5a678b8da415ae0715fcb2f2374734
alemrn <a.moreno@eimagina.net>
2021-03-24 08:22:50 +0100

    firebaseConfig

--- pushnotification/notificationapp/lib/main.dart
+++ pushnotification/notificationapp/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:notificationapp/src/pages/home_page.dart';
 import 'package:notificationapp/src/pages/mensaje_page.dart';
@@ -6,7 +7,17 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) async {
+    FirebaseMessaging messaging = FirebaseMessaging.instance;
+    NotificationSettings settings = await messaging.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+      sound: true,
+    );
     return MaterialApp(
       title: 'Material App',
       initialRoute: 'home',
