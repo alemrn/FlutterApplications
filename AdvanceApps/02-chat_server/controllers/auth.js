b1811cc5ef3e7e56a6c4db775c46c2ae4949affc
alemrn <amorenoechevarria@gmail.com>
2021-05-30 14:11:01 +0200

    guardar usuarios en la base de datos de Mongo DB

--- AdvanceApps/02-chat_server/controllers/auth.js
+++ AdvanceApps/02-chat_server/controllers/auth.js
@@ -1,11 +1,31 @@
 const { response } = require("express");
-const { validationResult } = require("express-validator");
+const Usuario = require("../models/usuario.js");
 
-const crearUsuario = (req, res = response) => {
-  res.json({
-    ok: true,
-    msg: "Crear usuario",
-  });
+const crearUsuario = async (req, res = response) => {
+  const { email, password, nombre } = req.body; //Desestructuración
+
+  try {
+    const existeEmail = await Usuario.findOne({ email });
+    if (existeEmail) {
+      return res.status(400).json({
+        ok: false,
+        msg: "El correo ya está registrado",
+      });
+    }
+    const usuario = new Usuario(req.body);
+    await usuario.save();
+
+    res.json({
+      ok: true,
+      usuario,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Hable con el administrador",
+    });
+  }
 };
 
 module.exports = { crearUsuario };
